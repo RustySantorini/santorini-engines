71de5a46444dc4ecd6cee3d5bc0879171611d716
Rafael <rafaelpmeirelles@gmail.com>
2024-01-15 19:52:20 -0300

    [#36] Hashing and unhashing workers

--- engines/src/helpers.rs
+++ engines/src/helpers.rs
@@ -42,3 +42,41 @@ pub(crate) mod workers {
     pub const U1: usize = 2;
     pub const U2: usize = 3;
 }
+
+pub fn hash_workers(workers:[usize;4]) -> usize{
+    let mut sum:usize = 0;
+    let base:usize = 25;
+    for i in 0..4{
+        sum += base.pow(i) * workers[i as usize];
+    }
+    sum
+}
+
+pub fn unhash_workers(mut hash: usize) -> [usize; 4] {
+    let mut w: [usize; 4] = [0; 4]; // Initialize the array with zeros
+    let base: usize = 25;
+    for i in 0..4 {
+        w[i] = hash % base;
+        hash /= base;
+    }
+    w
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::helpers::squares::*;
+
+    use super::*;
+    
+    #[test]
+    fn hashing(){
+        let w1 = [A1, A2, A3, A4];
+        let w2 = [C3, C4, B3, D3];
+        let w3 = [A1, A5, E1, E5];
+        for i in [w1, w2, w3]{
+            assert_eq!(i, unhash_workers(hash_workers(i)));
+        }
+    }
+
+}
