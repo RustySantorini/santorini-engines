7b3e5b8606e85a82cd980467ade958dba4aba20e
Rafael <rafaelpmeirelles@gmail.com>
2024-01-16 00:09:56 -0300

    [#36] Simple benchmarking

---
+++ benchmarking/src/main.rs
@@ -0,0 +1,15 @@
+use benchmark::benchmark::run_test;
+use engines::Board;
+
+mod sql_helpers;
+mod benchmark;
+
+fn main() {
+    let mut depth = 4;
+    loop{
+        for _ in 0..10{
+            run_test(1, 1, depth);
+        }
+        depth += 1;
+    }
+}
